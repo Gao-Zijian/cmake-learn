7404131d03ff80514edf8a943bef13df30d26bb0
Gao-Zijian <Gao-Zijian@noreply.gitcode.com>
2026-01-17 16:30:20 +0800

    初始化

---
+++ chap1/102cmake_lib/xlog/xlog.h
@@ -0,0 +1,21 @@
+// xlog.h
+
+#ifndef XLOG_H__
+#define XLOG_H__
+
+#ifndef _WIN32
+	#define XCPP_API
+#else
+	#ifdef xlog_EXPORTS
+		#define XCPP_API __declspec(dllexport)
+	#else 
+		#define XCPP_API __declspec(dllimport)
+	#endif
+#endif
+
+class XCPP_API XLog
+{
+public:
+	XLog();
+};
+#endif
